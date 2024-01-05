dbea357393de416e09cdf98852ead902040d26e6
alishan <alishan@10.0.0.226>
2024-01-05 13:34:20 -0500

    added more print sections

--- src/h/DemoH.java
+++ src/h/DemoH.java
@@ -4,10 +4,11 @@ public class DemoH {
 public static void main(String[] args) {
 	
 	System.out.println("DemoTester");
+	System.out.println("a / b");
 }
 	public static void sub(int a, int b){
 		System.out.println("a - b");
-		system.out.println(a / b);
+		System.out.println(a / b);
 		
 	}
 }
